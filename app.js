1b27344f06cfe7e8166acceffd76ef912d85e7d4
Siva Kumar <siva.peketi@resolver.com>
2021-05-22 14:34:41 +0530

    sample code to use post method and in built body parset json

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser'); // now body-parser is coming bydefault with express no need of separate import/install body-parser
 
 const server = express();
+
+server.use(express.json()); // to parse json type data
+server.use(express.urlencoded({extended:true})); // TODO find alternative to depricated body-parser
+
+server.use('/addUsers', (req, res, next)=> {
+    console.log('I\'m in the middleware');
+    res.send(`<form action=\'/addUser\' method=\'POST\'> 
+    <input type=\'text\' name=\'userName\'>
+    <input type=\'submit\' value=\'Add User\'></form>`);
+});
+
+server.post('/addUser', (req, res, next)=> {
+    console.log(req.body);
+    console.log(req.body.name);
+    res.send('<h1> User Added');
+});
 
 server.use('/', (req, res, next)=> {
     console.log('I\'m in the middleware');
