bc22bcab27595c6abf4b8cbbb64726cc830b4bb4
Siva Kumar <siva.peketi@resolver.com>
2021-05-22 19:15:48 +0530

    static content support added

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
+
 const bodyParser = require('body-parser'); // now body-parser is coming bydefault with express no need of separate import/install body-parser
 
 const adminRouter = require('./routes/admin');
+const productRouter = require('./routes/product');
+const shopRouter = require('./routes/shop');
 const defaultRouter = require('./routes/default');
 
 const server = express();
@@ -9,8 +13,10 @@ const server = express();
 server.use(express.json()); // to parse json type data
 server.use(express.urlencoded({extended:true})); // TODO find alternative to depricated body-parser
 
+server.use(express.static(path.join(__dirname, 'public')));
 server.use('/admin', adminRouter); //order is important
-server.use(defaultRouter);  //this listner always should be last as its request path matches with any request start with '/'
+server.use('/product', productRouter)
+server.use(shopRouter);  //this listner always should be last as its request path matches with any request start with '/'
 
 server.use((req,ress, next) => {
   ress.status(404).send('<h1> Page Not Found</h2>');
--- routes/admin.js
+++ routes/admin.js
@@ -2,21 +2,68 @@ const express = require('express');
 
 const router = express.Router();
 
-// /admin/addUser => GET
-router.get('/addUser', (req, res, next)=> {
-    console.log('I\'m in the middleware');
-    res.send(`
-    <form action="/admin/addUser" method="POST"> 
-        <input type="text" name="userName" placeholder="User Name">
-        <input type="submit" value="Add User">
-    </form>`);
-});
+const addUserGETScript = `<form class="product-form" action="/admin/addUser" method="POST"> 
+                        <div class="form-control">
+                            <label for="title">Title</label>
+                            <input type="text" name="userName" placeholder="User Name">
+                        </div>
+                        <input type="submit" value="Add User">
+                    </form>`;
+const adminHtml = `
+<!DOCTYPE html>
+<html lang="en">
 
-// /admin/addUser => POST
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Add Product</title>
+    <link rel="stylesheet" href="/css/main.css">
+    <link rel="stylesheet" href="/css/product.css">
+</head>
+
+<body>
+    <header class="main-header">
+        <nav class="main-header__nav">
+            <ul class="main-header__item-list">
+                <li class="main-header__item"><a href="/">Shop</a></li>
+                <li class="main-header__item"><a class="active" href="/product/add-product">Add Product</a></li>
+                <li class="main-header__item"><a href="/admin/addUser">Add User</a></li>
+            </ul>
+        </nav>
+    </header>`;
+
+const addUserHTMLScript = `<form action="/admin/addUser" method="POST"> 
+                                <input type="text" name="userName" placeholder="User Name">
+                                <input type="submit" value="Add User">
+                            </form>`;
+
+/**
+ *   /admin/addUser => GET
+ **/
+router.get('/addUser', (req, res, next)=> {
+    res.send(
+        `${adminHtml} 
+        <main>
+            ${addUserGETScript}
+        </main>
+        </body>
+        </html>`
+)});
+
+/**
+ *   /admin/addUser => POST
+ **/
 router.post('/addUser', (req, res, next)=> {
     console.log(req.body);
     console.log(req.body.userName);
-    res.send('<h1> User Added');
-});
+    res.send(
+        `${adminHtml} 
+        <main>
+            <center><h2>User ${req.body.userName} added </center>
+        </main>
+        </body>
+        </html>`
+)});
 
 module.exports = router;
